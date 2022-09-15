8900a72207c742e9703700d5b1631eb616329467
Nicolas Molina Monroy <hi@nicobytes.com>
2022-09-15 12:21:14 -0400

    :construction: Work in exercise

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import "./styles.css";
 import { runCode } from "./exercise";
 
 (() => {
-  const arrayA = [1, 2, 3, 4];
-  const arrayB = [5, 6, 7, 8];
-  const rta = runCode(arrayA, arrayB);
-
   document.getElementById("app").innerHTML = `
-  <h1>Title</h1>
-  <p>Array A: <code>${arrayA}</code></p>
-  <p>Array B: <code>${arrayB}</code></p>
-  <p>Response from <code>runCode</code>
-  <pre><code>${rta}</code></pre>
+    <h1>Callback</h1>
+    <p>Response from <code>runCode</code></p>
+    <pre><code id="code"></code></pre>
   `;
+
+  const callback = () => {
+    document.getElementById('code').innerHTML = 'run after 2s';
+  }
+
+  runCode(callback);
+
 })();
